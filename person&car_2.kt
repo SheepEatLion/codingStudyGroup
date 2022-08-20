dc6da54479fa19ccc49c99cd72a2f0e5939aa4d2
Choijiwoong <gogogi313@naver.com>
2022-08-20 17:05:23 +0900

    [DOCS] 불필요 기능 제거, go와 stop의 예외처리를 위해 is_run변수 도입

--- person&car_2.kt
+++ person&car_2.kt
@@ -1,5 +1,3 @@
-import kotlin.properties.Delegates
-
 /*
 기획서
 1. 사람이 있다.
@@ -12,39 +10,49 @@ import kotlin.properties.Delegates
 8. 사람은 여러명일 수 있다.
 9. 자동차는 출발하거나 정지할 수 있다.
 */
-abstract class Vehicle{
+abstract class Vehicle{//출발과 정지 기능이 있는 탈것 추상클래스
     abstract fun go()
     abstract fun stop()
 }
 
 class Car(whichCar: String): Vehicle() {
-    companion object{
+    companion object{//차의 종류를 List로 저장한다.
         val carCategory = listOf<String>("Hyundai", "Kia")//차는 두종류로 한정.
     }
-    var carName:String="null"
+    var carName:String
+    var is_run=false
     init{
-        if(whichCar in carCategory || whichCar=="null"){
-             carName=whichCar
-        } else{
+        carName = if(whichCar in carCategory || whichCar=="null"){
+            whichCar
+        } else{//차의 종류를 만족시키지 못했을 경우 null을 default로 넣는다.
             println("Wrong car name!")
-            carName="null"
+            "null"
         }
     }
 
     override fun go(){
         if(carName=="null") {
-            println("Make car first!")
+            println("Buy a car first!")
             return
         }
-
+        if(is_run==true){
+            println("$carName is already running!")
+            return
+        }
+        is_run=true
         println("$carName go!")
     }
 
     override fun stop(){
         if(carName=="null") {
-            println("Make car first!")
+            println("Buy a car first!")
             return
         }
+        if (is_run==false){
+            println("$carName is already stopped!")
+            return
+        }
+        is_run=false
         println("$carName stop!")
     }
 }
@@ -52,10 +60,6 @@ class Car(whichCar: String): Vehicle() {
 open class basicInformation(name: String, age: Int){
     protected var _name=name
     protected var _age=age
-
-    protected fun printBasicInformation(){
-        println("name is $_name, age is $_age")
-    }
 }
 
 interface Purchaseable{
@@ -63,30 +67,39 @@ interface Purchaseable{
 }
 
 class Person constructor(name: String, age: Int): basicInformation(name, age), Purchaseable{
-    var has_car=false
     var car:Car=Car("null")
 
     override fun purchase(name: String) {
-        if(car.carName!="null"){
+        if(car.carName!="null"){//이미 차를 갖고있는 경우
             println("you already have a car!")
             return
         }
 
-        car=Car(name)
-        if(car.carName!="null"){
+        if(name in Car.carCategory){//결제하려는 차종이 결제가능한 차종인디 확인.
+            car=Car(name)
             println("success for purchase $name!")
         }else{
-            println("fail to purchase a car!")
+            println("fail to purchase a car! you can only buy ${Car.carCategory}")
         }
     }
-
 }
 
 fun main(){
     var one=Person("choi", 22)
     var two=Person("tae", 28)
 
-    one.car.go()
+    println("정상적인 접근")
     one.purchase("Kia")
     one.car.go()
+    one.car.stop()
+
+    println("\n비정상적인 접근")
+    two.car.go()
+    two.purchase("null")
+    two.purchase("Fuck you")
+    two.purchase("Hyundai")
+    two.car.stop()
+    two.car.go()
+    two.car.go()
+    two.purchase("Kia")
 }
