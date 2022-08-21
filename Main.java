83639ee6d8a4dddd2d83fd9a07053af381de79e3
jerry8534 <109216969+jerry8534@users.noreply.github.com>
2022-08-21 15:02:27 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -19,14 +19,16 @@ public class Main{
         person.setCarPossession(in.next());
         System.out.print("1. 차 구매 2. 차고 가기 3. 탑승");
         person.setAct(in.next());
+        BMW bmw = new BMW();
+        KIA kia = new KIA();
         if(person.getAct().equals("1")) {
             if (person.getCarPossession().equals("NO")) {
                 System.out.print("구매하시고 싶은 차를 선택하세요 1. BMW 2. KIA");
                 int want = in.nextInt();
                 if (want == 1) {
-                    System.out.print("당신은 BMW를 구매하셨습니다!");
+                    System.out.print("당신은 bmw를 구매하셨습니다!");
                         else{
-                        System.out.print("당신은 KIA를 구매하셨습니다!");
+                        System.out.print("당신은 kia를 구매하셨습니다!");
                     }
                 }
             } else {
@@ -36,10 +38,31 @@ public class Main{
         else if(person.getAct().equals("2")){
             System.out.print(person.getCarPossession());
             }
+        else{
+            if (person.getCarPossession().equals("NO")) {
+                System.out.print("가지고 있는 차가 없습니다.");
+                else if (person.getCarPossession().equals("BMW")) {
+                    System.out.print("1. 출발하기 2. 정지하기");
+                    int do = in.nextInt();
+                    if(do == 1){
+                        bmw.start();
+                    }
+                    else{
+                        bmw.stop();
+                    }
+                }
+            }
+                else{
+                    System.out.print("1. 출발하기 2. 정지하기");
+                    int do = in.nextInt();
+                    if(do == 1){
+                        kia.start();
+                    }
+                    else{
+                        kia.stop();
+                    }
+                }
         }
-        else{어떤 차 선택?
-            bmw
-        bmw.start
     }
 
 }
